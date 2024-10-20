3c5b28ba2031fd16d773b1054049c96a15f21db2
Erik Villegas <erikvillegas1@gmail.com>
2024-10-19 19:57:00 -0700

    update

--- src/notifications/NotificationSender.ts
+++ src/notifications/NotificationSender.ts
@@ -1,5 +1,11 @@
 import webpush, { PushSubscription } from "web-push";
 
+webpush.setVapidDetails(
+    "mailto: <erikvillegas1@gmail.com>",
+    "BI7gXKy0oG_RZBdrFR--AH-2XfUQhhayayLFDvlx-P7CndhekSuisYYSCDiisidGz2FXL_-v47H-bwoAFBQRH5k",
+    "MTd78uR1Qs2c5FXBd1uHX7AKRdM-pBp6wrFmFE9Whu4"
+);
+
 export const sendNotification = async (subscription: PushSubscription, title: string, message: string) => {
     const pushPayload: any = {
         title: title,
